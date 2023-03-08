4a39a77de12a4cc09c6c7b47bf42433c6c9d8b84
cristianranged <crangeld@ufpso.edu.co>
2023-03-07 21:22:50 -0500

    implementar arquitectura y entidad de db

---
+++ src/main/java/com/procesos/inventario/services/UserService.java
@@ -0,0 +1,20 @@
+package com.procesos.inventario.services;
+
+import com.procesos.inventario.models.User;
+import com.procesos.inventario.respository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public Optional getUser(Long id){
+
+        return userRepository.findById(id);
+    }
+}
