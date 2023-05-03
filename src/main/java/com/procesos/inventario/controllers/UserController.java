4e51aea5d62fcdcdc0cc7256e2da6ba2d3b3347f
cristianranged <crangeld@ufpso.edu.co>
2023-05-02 21:02:04 -0500

    Reestructuración de mensajes de la api

--- src/main/java/com/procesos/inventario/controllers/UserController.java
+++ src/main/java/com/procesos/inventario/controllers/UserController.java
@@ -1,7 +1,10 @@
 package com.procesos.inventario.controllers;
 
 import com.procesos.inventario.models.User;
+import com.procesos.inventario.services.UserService;
 import com.procesos.inventario.services.UserServiceImp;
+import com.procesos.inventario.utils.ApiResponse;
+import com.procesos.inventario.utils.Constants;
 import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,68 +14,75 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
-    private UserServiceImp userServiceImp;
+    private UserService userService;
+    private ApiResponse apiResponse;
+    Map data = new HashMap<>();
 
-    @GetMapping(value = "/user/{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity findUserById(@PathVariable Long id){
-        Map response = new HashMap();
-
-
         try {
-            return new ResponseEntity(userServiceImp.getUser(id), HttpStatus.OK);
-        }catch(Exception e){
-            response.put("status","404");
-            response.put("message","No se encontro el usuario");
-            return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
+            apiResponse = new ApiResponse(Constants.REGISTER_FOUND,(userService.getUser(id)));
+            return new ResponseEntity(apiResponse,HttpStatus.OK);
+            }catch(Exception e)
+            {
+            apiResponse = new ApiResponse(Constants.REGISTER_NOT_FOUND,"");
+            return new ResponseEntity(apiResponse,HttpStatus.NOT_FOUND);
             }
 
     }
-
-    // TAREA
-    @GetMapping(value = "/users")
-    public ResponseEntity allUsers(){
-        Map response = new HashMap();
-        try {
-            return new ResponseEntity(userServiceImp.allUsers(), HttpStatus.OK);
-        }catch(Exception e){
-            response.put("status","404");
-            response.put("message","No existen usuarios ");
-            return new ResponseEntity<>(response,HttpStatus.NOT_FOUND);
-        }
-
-    }
-    //
-
-    @PostMapping (value = "/user")
+    @PostMapping (value = "")
     public ResponseEntity saveUser(@RequestBody User user){
-        Map response = new HashMap();
-        Boolean userResp = userServiceImp.createUser(user);
+        Boolean userResp = userService.createUser(user);
 
         if (userResp == true){
-            response.put("status","201");
-            response.put("message"," se creo  el usuario");
-            return new ResponseEntity<>(response,HttpStatus.CREATED);
+           apiResponse = new ApiResponse(Constants.REGISTER_CREATED,"");
+           return  new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
         }
-        response.put("status","404");
-        response.put("message","Hubo un error al crear el usuario");
-        return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+        apiResponse = new ApiResponse(Constants.REGISTER_BAD,user);
+        return new ResponseEntity<>(apiResponse,HttpStatus.BAD_REQUEST);
+    }
+    // TAREA
+    @GetMapping(value = "")
+    public ResponseEntity allUsers(){
+
+        try {
+            apiResponse = new ApiResponse(Constants.REGISTER_LOGIN,userService.allUsers());
+            return new ResponseEntity(apiResponse,HttpStatus.OK);
+        }catch(Exception e){
+            apiResponse = new ApiResponse(Constants.REGISTER_NOT_FOUND,"");
+            return new ResponseEntity(apiResponse,HttpStatus.NOT_FOUND);
+        }
+
     }
 
+/*
+    @PutMapping(value = "/user/{id}")
+    public ResponseEntity updateUser(@PathVariable Long id, @RequestBody User user) {
 
-    @PostMapping (value = "auth/login")
-    public ResponseEntity login(@RequestBody User user){
-            Map response = new HashMap();
-            try {
-                return new ResponseEntity(userServiceImp.login(user), HttpStatus.OK);
-            }catch  (Exception e){
-                response.put("status","404");
-                response.put("message",e.getMessage());
-                return new ResponseEntity<>(response,HttpStatus.BAD_REQUEST);
+        Boolean userDB = userServiceImp.updateUser(id, user);
+        try {
+            if (userDB == null) {
+
+                apiResponse = new ApiResponse(Constants.REGISTER_UPDATED,"");
+                return new ResponseEntity(apiResponse, HttpStatus.BAD_REQUEST);
             }
+            return new ResponseEntity(apiResponse.getUser(id), HttpStatus.ACCEPTED);
+        } catch (Exception e) {
+            response.put("status", "201");
+            response.put("message", "se encontro usuario");
+            return new ResponseEntity(response, HttpStatus.BAD_REQUEST);
+        }
     }
+
+
+*/
+
+
+
 
 
 }
